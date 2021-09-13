50e0d50eee3a3e137a0940817bd4dc181104bd43
yevgeller <yevgeller@gmail.com>
2021-09-12 19:24:46 -0500

    Beginning of a WebAPI project

---
+++ DMS.API/DMS.API/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using DMS.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAllStudents")]
+        public IEnumerable<Student> GetAll()
+        {
+            return new List<Student>
+            {
+                new Student{Student_Id = 1, First_Name="John", Last_Name = "Jones"},
+                new Student{Student_Id=2, First_Name = "Mary", Last_Name = "Jones"},
+                new Student{Student_Id =3, First_Name="Kelly", Last_Name="Michaels"}
+            };
+        }
+    }
+}
---
+++ DMS.API/DMS.API/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.API.Models
+{
+    public class Student
+    {
+        public int Student_Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+    }
+}
